0a40c135a09cc69438e1905aa6165bc921f103e7
jeffreymzd <mzhde@hotmail.com>
2020-03-15 22:30:32 -0400

    Adding Put/Post/Delete RestTemplate examples and JUnit test

--- src/test/java/com/github/jeffrey/spring/boot/sfgmsscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,8 +21,36 @@ class BreweryClientTest {
 
     @Test
     void getBeerById() {
+
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+
+        System.out.println(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri);
+    }
+
+    @Test
+    void updateBeer() {
+
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
     }
 }
